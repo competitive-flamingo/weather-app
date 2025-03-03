9b2ee1593005ceb124edb7ad90f965b1d8fd8a77
Marwan Esam <competitiveflamingo@gmail.com>
2025-03-04 00:59:53 +0200

    Fix Webpack config for GitHub Pages

--- webpack.common.js
+++ webpack.common.js
@@ -11,6 +11,7 @@ module.exports = {
     filename: "[name].js",
     path: path.resolve(__dirname, "dist"),
     clean: true,
+    publicPath: '/weather-app/',
   },
   plugins: [
     new HtmlWebpackPlugin({
