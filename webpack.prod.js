54292f6f75b437582e0e0291ee96b43ef2906fd5
Marwan Esam <competitiveflamingo@gmail.com>
2025-03-04 01:55:02 +0200

    fix configs

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,23 +1,6 @@
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = merge(common, {
   mode: 'production',
-  // module: {
-  //   rules: [
-  //     {
-  //       test: /\.css$/i,
-  //       use: [MiniCssExtractPlugin.loader, 'css-loader'],
-  //     },
-  //   ],
-  // },
-  // plugins: [
-  //   new MiniCssExtractPlugin({
-  //     filename: 'weatherData.css',
-  //   }),
-  //   new MiniCssExtractPlugin({
-  //     filename: 'index.css',
-  //   }),
-  // ],
 });
